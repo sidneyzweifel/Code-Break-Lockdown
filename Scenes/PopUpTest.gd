e679f00f3a857a365b32d437227b00691b30db87
ogorsr <ogorsr@protonmail.com>
2024-01-20 17:44:22 -0500

    Simple PopUp
    Start with popup that user can get rid of by clicking the x at the corner. Still have some sizing issues and still need to consider how to turn the player's movement off when in the pop up window. Goal is to have player start in the cell and be able to click on things in the cell that allow it to exit the cell and then enter the mode where they can move around.

---
+++ Scenes/PopUpTest.gd
@@ -0,0 +1,21 @@
+extends Window
+
+#current code from this video: https://youtu.be/qo8SDCuVCoY?si=Bzc50yOhnVVNSWqR
+
+@onready var popup_test_ = $"."
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	popup_test_.show()
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+
+func _on_close_requested():
+	popup_test_.hide()
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var speed = 400
+@export var speed = 800 #was going too slow for me
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
