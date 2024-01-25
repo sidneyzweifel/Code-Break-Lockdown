d997cb4c34148039fc53ea7ad2771300fc792391
sidneyzweifel <sidneyzweifel1@gmail.com>
2024-01-25 12:48:55 -0500

    Scene Switching
    1 new global script, 8 new local scripts (2 per puzzle)

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+func _ready():
+	pass
+	
+func _process(delta):
+	pass
 
-func _on_go_to_logic_puzzle_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Logic_Puzzle_Sidney.tscn")
+
+
---
+++ Scripts/Variables_Puzzle_Exit.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+# If player collides with Area2D, change scene
+func _on_body_entered(body):
+	if body.name == "Player":
+		
+		# Scene name and position (x,y)
+		Global.changeStage(Global.MAIN, 907, 888)
