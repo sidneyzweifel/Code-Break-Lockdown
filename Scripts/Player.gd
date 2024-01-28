d628e340afe4074924cc35121fa166ee727ebbc2
sidneyzweifel <sidneyzweifel1@gmail.com>
2024-01-28 15:20:47 -0500

    Revert "Merging Gretta's animations"
    This reverts commit 1dff05920ee2630563984a06af63f6399d0bec3e, reversing
    changes made to 05020e48e8389d27bfd9dd9e64e4896624b3d8e8.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,39 +1,11 @@
 extends CharacterBody2D
-@onready var animated_sprite = $AnimatedSprite2D
-var direction = ""
+
 @export var speed = 400
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
-	
-	if Input.is_action_pressed("left"):
-		animated_sprite.play("walking_side")
-		animated_sprite.flip_h = true
-		direction = "l"
-	
-	elif Input.is_action_pressed("right"):
-		animated_sprite.play("walking_side")
-		animated_sprite.flip_h = false
-		direction = "r"
-	
-	elif Input.is_action_pressed("up"):
-		animated_sprite.play("idle_up")
-		direction = "u"
-	
-	elif Input.is_action_pressed("down"):
-		animated_sprite.play("walking_front")
-		direction = "d"
-	
-	else:
-		if(direction == "l" or direction == "r"):
-			animated_sprite.play("idle_side")
-		elif direction == "d":
-			animated_sprite.play("idle_down")
-		elif direction == "u":
-			animated_sprite.play("idle_up")
-			
-				
+
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
