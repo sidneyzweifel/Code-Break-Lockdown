53ffaf659aafa7a655c4bb6f3e5ceca0c12ab56a
ogorsr <ogorsr@protonmail.com>
2024-02-04 19:17:17 -0500

    trying to get sprite added

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,12 +3,40 @@ extends CharacterBody2D
 @export var speed = 800 #was going too slow for me so I changed speed
 
 @onready var door_passcode = "res://WIP_Scences/passcode_enter.tscn"
+@onready var animated_sprite = $AnimatedSprite2D
+var direction = ""
 
 var player_position
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
+	
+	if Input.is_action_pressed("left"):
+		animated_sprite.play("walking_side")
+		animated_sprite.flip_h = true
+		direction = "l"
+	
+	elif Input.is_action_pressed("right"):
+		animated_sprite.play("walking_side")
+		animated_sprite.flip_h = false
+		direction = "r"
+	
+	elif Input.is_action_pressed("up"):
+		animated_sprite.play("walking_back")
+		direction = "u"
+	
+	elif Input.is_action_pressed("down"):
+		animated_sprite.play("walking_front")
+		direction = "d"
+	
+	else:
+		if(direction == "l" or direction == "r"):
+			animated_sprite.play("idle_side")
+		elif direction == "d":
+			animated_sprite.play("idle_down")
+		elif direction == "u":
+			animated_sprite.play("idle_up")
 
 
 func _physics_process(delta):
