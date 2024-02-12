7c7ef14227f2039e13fc4b8afd2d6fed291ea504
ogorsr <ogorsr@protonmail.com>
2024-02-12 10:39:02 -0500

    Added Player No Cam for Puzzle 1
    Still formatted strange. How can we see what it will look like fullscreen intially

--- WIP_Scences/door.gd
+++ WIP_Scences/door.gd
@@ -7,8 +7,8 @@ class_name Door extends StaticBody2D
 #var passcode_scene = preload("res://WIP_Scences/passcode_enter.tscn")
 
 @export var path_to_new_scene:String
-@export var door_passcode_scene:String
-@export var current_puzzle:String
+@export var passcode_scene:String
+@export var path_current_puzzle:String
 
 #@onready var passcode_scene:PackedScene = preload("res://WIP_Scences/passcode_enter.tscn")
 
@@ -23,7 +23,7 @@ func handle_access(door_access):
 	if door_access:
 		go_to_next_puzzle()
 	else:
-		Global.go_to_passcode(door_passcode_scene, current_puzzle, path_to_new_scene)
+		Global.go_to_passcode(passcode_scene, path_current_puzzle, path_to_new_scene)
 #		var passcode_s = passcode_scene.instantiate()
 #		add_child(passcode_s)
 	return
