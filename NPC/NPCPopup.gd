c3132d3415de3baaf0dc64ee31e7347a38d198d0
ogorsr <ogorsr@protonmail.com>
2024-02-27 23:18:56 -0500

    Working on loop logic

--- NPC/NPCPopup.gd
+++ NPC/NPCPopup.gd
@@ -9,6 +9,7 @@ var dialouge_cnt:int = 0
 var player_dia_cnt:int = 0
 var next_option_button:Button
 
+
 @export var inmate_key:String
 
 
@@ -17,7 +18,7 @@ func _ready():
 	label_text.text = str(dia)
 	var response =  pick_next_player_response(player_dia_cnt)
 	next_option_button = popup.add_button(response, false, "custom action")
-
+	
 
 
 
@@ -35,7 +36,7 @@ func pick_next_dialouge_inmate(cnt:int):
 		2:
 			var color = Dialouge.speech_dict[inmate_key]["door_color"]
 			var pos = Dialouge.speech_dict[inmate_key]["door_location"]
-			return "It's " + str(pos) + " and is " + str(color)
+			return "The door is " + str(pos) + " and is " + str(color)
 		_:
 			return str("Please get me item #***")
 			
@@ -56,7 +57,7 @@ func pick_next_player_response(cnt:int):
 
 func _on_custom_action(action):
 	dialouge_cnt += 1
-	player_dia_cnt += 1
+	player_dia_cnt += 1	
 	var dia = pick_next_dialouge_inmate(dialouge_cnt)
 	label_text.text = str(dia)
 	var response = pick_next_player_response(player_dia_cnt)
--- Puzzles/loops_puzzle/loop_window.gd
+++ Puzzles/loops_puzzle/loop_window.gd
@@ -3,52 +3,74 @@ extends Window
 @onready var window = $"."
 @export var item_number:String
 @onready var item_number_label = $ItemNumLabel
+@onready var start_index_label = $StartIndex
 
-var start_index:String
+var start_index:int
 var end_index:String
 var cur_puzz:String
+var _increment_index:int = 0
+var _inequality_index:int = 0
 
 
+var rng = RandomNumberGenerator.new()
 func _on_close_requested():
 	window.hide()
 	
 
 func _ready():
 	item_number_label.text = "ITEM #: " + item_number
-	#cur_puzz = Global.get_current_puzzle()
+	start_index = rng.randi_range(100, 999)
+	start_index_label.text = str(start_index)
 	
 
 
 	
 	
 	
-func calculate_loop(start, end):
+func calculate_loop(start, end_index, inequality_index, increment_index):
 	print("Calculating....")
+	
+	
 
 
+func index_to_symbol_ineqaulities(index:int):
+	match index:
+		0:
+			return "increment"
+		1:
+			return "decrement"
+
+func index_to_symbol_inequalities(index:int):
+	match index:
+		0: 
+			return "less_than"
+		1:
+			return "greater_than"
+		2:
+			return "less_than_or_equal"
+		3:
+			return "greater_than_or_equal"
 
 
 func _on_button_pressed():
 	print("ENTERED")
 	print(start_index)
 	print(end_index)
+	calculate_loop(start_index, end_index, _inequality_index, _increment_index)
 	Global.handle_door(true)
 	pass # Replace with function body.
 
 
 func _on_inequality_item_selected(index):
+	_inequality_index = index
 	print(index)
 	pass # Replace with function body.
 
 
 func _on_increment_or_decrement_item_selected(index):
+	_increment_index = index
 	print(index)
 
-
-func _on_start_index_text_changed(new_text):
-	print("START")
-	print(new_text)
-	start_index = new_text
 
 
 func _on_end_index_text_changed(new_text):
