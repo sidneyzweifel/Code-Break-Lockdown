87eb7440981702f3c843b99a006aced29122c46f
Gretta Foxx <93896250+grFoxx@users.noreply.github.com>
2024-04-03 13:18:25 -0400

    add animations to title screen and terminal

--- Puzzles/doorLightColor.gd
+++ Puzzles/doorLightColor.gd
@@ -2,22 +2,35 @@ extends ColorRect
 
 var clr = "red"
 
-var greenColor = Color(0, 63, 255, .85)
+var greenColor = Color("00e600e0")
+var greenLightColor = Color("00df00c2")
+
 var redColor = Color(0, 0, 0, 0)
-var blueColor = Color(0, 0, 255, .85)
-var yellowColor = Color(255, 255, 0, .85)
+var redLightColor = Color("ff0000c2")
+
+var blueColor = Color("003bffe0")
+var blueLightColor = Color("0008fac2")
+
+var yellowColor = Color("ffff0bae")
+var yellowLightColor = Color("ffff3ae4")
+
+var purpleColor = Color("0000ffa6")
+var purpleLightColor = Color("c900b4")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if clr == "yellow":
 		%doorLightColor.set_modulate(yellowColor)
-		#%lightGlow.set_color(yellowColor)
+		%lightGlow.set_color(yellowLightColor)
 	elif clr == "green":
 		%doorLightColor.set_modulate(greenColor)
-		#%lightGlow.set_color(yellowColor)
-	elif clr == "blue": # doesnt work
+		%lightGlow.set_color(greenLightColor)
+	elif clr == "blue":
 		%doorLightColor.set_modulate(blueColor)
-		#%lightGlow.set_color(yellowColor)
+		%lightGlow.set_color(blueLightColor)
 	elif clr == "red":
 		%doorLightColor.set_modulate(redColor)
-		#%lightGlow.set_color(yellowColor)
+		%lightGlow.set_color(redLightColor)
+	elif clr == "purple":
+		%doorLightColor.set_modulate(purpleColor)
+		%lightGlow.set_color(purpleLightColor)
 
