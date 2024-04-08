346ad12fbd8ec30cfd937e9ecdc995da3e10f16e
ogorsr <ogorsr@protonmail.com>
2024-04-07 21:20:46 -0400

    I broke something :(

---
+++ Door_Puz4.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+
+
+#Script for each door
+#add doors to door group to work with player collison
+
+
+#Must provide for each door: the next scene after the door, a passcode scene, and the current scene 
+#that the puzzle is instationated on
+@export var path_to_new_scene:String
+@export var passcode_scene:String
+@export var path_current_puzzle:String
+
+
+#door group function to determine if player can move to next puzzle
+func handle_access(door_access):
+	if door_access:
+		pass
+	else:
+		Global.go_to_passcode(passcode_scene, path_current_puzzle, path_to_new_scene)
+	return
+
+
+func _on_button_pressed():
+	handle_access(false)
+
+
+func _on_ready():
+	print("DOOR PUZ 4 READy")
