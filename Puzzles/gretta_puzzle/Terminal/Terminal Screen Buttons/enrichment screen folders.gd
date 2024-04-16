b810646d58af7408fe285ac8cfafbcf20a8c31c5
Gretta Foxx <93896250+grFoxx@users.noreply.github.com>
2024-04-15 23:03:26 -0400

    add terminal functionality

--- Puzzles/gretta_puzzle/Terminal/Terminal Screen Buttons/enrichment screen folders.gd
+++ Puzzles/gretta_puzzle/Terminal/Terminal Screen Buttons/enrichment screen folders.gd
@@ -3,6 +3,14 @@ extends Node2D
 var backToScreen = "res://Puzzles/gretta_puzzle/Terminal/Terminal Screen Scenes/Prisoner Enrichment Screen.tscn"
 var previousScreen ="res://Puzzles/gretta_puzzle/Terminal/Terminal Screen Scenes/Home Terminal Screen.tscn"
 
+var previousChild = 0
+
+func _ready():
+	%Window.hide()
+	%enrichment.get_child(0).hide()
+	%enrichment.get_child(1).hide()
+	%enrichment.get_child(2).hide()
+	
 func _on_prisoner_enrichment_screen__line_edit_text_submitted(new_text):
 
 	# put input to all lowercase to ensure no issues with case sensitivity
@@ -11,13 +19,20 @@ func _on_prisoner_enrichment_screen__line_edit_text_submitted(new_text):
 	# go to the correct scene based on inputted text
 	if new_text == "print enrichment image 1":
 		#SceneTransition.change_scene_to_file(variable1) 
-		pass
+		%enrichment.get_child(previousChild).hide()
+		%Window.show()
+		%enrichment.get_child(0).show()
+		previousChild = 0
 	elif new_text == "print enrichment image 2":
-		#SceneTransition.change_scene_to_file(variable2)
-		pass
+		%enrichment.get_child(previousChild).hide()
+		%Window.show()
+		%enrichment.get_child(1).show()
+		previousChild = 1
 	elif new_text == "print enrichment image 3":
-		#SceneTransition.change_scene_to_file(variable3)
-		pass
+		%enrichment.get_child(previousChild).hide()
+		%Window.show()
+		%enrichment.get_child(2).show()
+		previousChild = 2
 	elif new_text == "back":
 		SceneTransition.change_scene_to_file(previousScreen)
 	else:
