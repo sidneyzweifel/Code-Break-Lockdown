f1c7bd78868fff8cf2b4d9a6c3a6bbc30f3105fc
ogorsr <ogorsr@protonmail.com>
2024-04-16 15:31:59 -0400

    Added inmate images
    inmate_B still temp

--- NPC/NPC.gd
+++ NPC/NPC.gd
@@ -16,7 +16,7 @@ var item_num = load("res://Puzzles/loops_puzzle/item_number/item_number.gd")
 func _ready():
 	button_.text = button_text
 	item_dict.add_item(inmate_key, num_item.item_num)
-	
+	$InmateSprite.texture = load("res://Backgrounds/" + str(inmate_key)+ ".png")
 
 func _on_button_pressed():
 	Puzzle4Autoload.inmate_clicked()
