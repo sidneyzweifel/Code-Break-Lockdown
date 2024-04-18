a65fd08254806cf527939b186d1c4ab658b822eb
sidneyzweifel <sidneyzweifel1@gmail.com>
2024-04-17 20:33:49 -0400

    Fixed bug in Passcode.gd
    passcode= Global.set_passcode() changed ->
    passcode= await Global.set_passcode()
    
    # await must be here or there will be an error
    
    also for some reason, the old and new contents of the ready function were both merged, so i changed it the modified version with the if-else

--- Popup/Passcode/Passcode.gd
+++ Popup/Passcode/Passcode.gd
@@ -7,16 +7,10 @@ extends Label
 # passcode every time i left the terminal scene and went back to 
 # the puzzle 1 scene, so i added a check if there already is a passcode
 func _ready():
-	var passcode =  await Global.set_passcode()
-	passcode_text.text = "PASSCODE: " + passcode
-	print("PASSCODE: " + passcode)
-	pass 
-
-
 	var passcode
 	if Global.get_passcode() : # if there is a passcode do nothing
 		pass
 	else: # otherwise set a passcode
-		passcode= Global.set_passcode()
+		passcode= await Global.set_passcode() # await must be here or there will be an error
 		passcode_text.text = "PASSCODE: " + passcode
 		print(passcode)
