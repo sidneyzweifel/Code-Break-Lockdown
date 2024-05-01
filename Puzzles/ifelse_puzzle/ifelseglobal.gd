fbd8006c71d16c6f21a38b30f29b46a5cf45dc9f
yxg310 <15yagu@gmail.com>
2024-05-01 12:45:29 -0400

    update

--- Puzzles/ifelse_puzzle/ifelseglobal.gd
+++ Puzzles/ifelse_puzzle/ifelseglobal.gd
@@ -1,9 +1,10 @@
-extends LineEdit
+extends Node
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
